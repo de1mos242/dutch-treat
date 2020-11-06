4d5090fb824f1a917b7f4bafa179371c8cf17d51
Denis Yakovlev <de1m0s242@gmail.com>
2020-11-07 03:18:15 +0700

    change to jetty

--- src/main/kotlin/net/de1mos/dutchtreat/endpoints/JaicpServletApi.kt
+++ src/main/kotlin/net/de1mos/dutchtreat/endpoints/JaicpServletApi.kt
@@ -1,17 +1,13 @@
 package net.de1mos.dutchtreat.endpoints
 
-import ch.qos.logback.core.LogbackException
 import com.justai.jaicf.BotEngine
 import com.justai.jaicf.channel.jaicp.JaicpServlet
 import com.justai.jaicf.channel.jaicp.JaicpWebhookConnector
 import com.justai.jaicf.channel.jaicp.channels.ChatWidgetChannel
 import com.justai.jaicf.model.scenario.Scenario
-import io.netty.util.internal.logging.Slf4JLoggerFactory
-import org.apache.logging.slf4j.SLF4JLogger
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import java.lang.RuntimeException
 
 val bot = object : Scenario() {
     init {
@@ -27,27 +23,16 @@ val engine = BotEngine(bot.model, activators = emptyArray())
 class JaicpServletApi {
 
     @Bean
-    fun jaicpServlet() {
-        val log = Slf4JLoggerFactory.getInstance(this.javaClass)
-        log.warn("We are about to start")
-        try {
-            ServletRegistrationBean(
-                    JaicpServlet(
-                            JaicpWebhookConnector(
-                                    botApi = engine,
-                                    accessToken = System.getenv("JAICP_API_TOKEN")
-                                            ?: "d38e8351-d1f3-4547-a2d8-b17237eb814a",
-                                    channels = listOf(ChatWidgetChannel)
-                            )
-                    ),
-                    "/*"
-            ).apply {
-                setLoadOnStartup(1)
-            }
-        } catch (e: RuntimeException) {
-            log.error("caught ex ${e.message}")
-        }finally {
-            log.warn("we are about to end")
-        }
+    fun jaicpServlet() = ServletRegistrationBean(
+            JaicpServlet(
+                    JaicpWebhookConnector(
+                            botApi = engine,
+                            accessToken = System.getenv("JAICP_API_TOKEN") ?: "d38e8351-d1f3-4547-a2d8-b17237eb814a",
+                            channels = listOf(ChatWidgetChannel)
+                    )
+            ),
+            "/*"
+    ).apply {
+        setLoadOnStartup(1)
     }
 }
