b53767d6abba6a4ff9246358f4d3ef66ebfa9cfc
Denis Yakovlev <de1m0s242@gmail.com>
2020-11-07 04:30:11 +0700

    without mongo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	application
 	id("org.springframework.boot") version "2.3.5.RELEASE"
+	application
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
@@ -38,18 +38,17 @@ extra["testcontainersVersion"] = "1.14.3"
 
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+//	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+//	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
 	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:mongodb")
+//	testImplementation("org.testcontainers:mongodb")
 
 	implementation("ch.qos.logback:logback-classic:$logback")
 
--- src/main/kotlin/net/de1mos/dutchtreat/DutchTreatApplication.kt
+++ src/main/kotlin/net/de1mos/dutchtreat/DutchTreatApplication.kt
@@ -1,9 +1,11 @@
 package net.de1mos.dutchtreat
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DispatcherServletAutoConfiguration::class, ErrorMvcAutoConfiguration::class])
 class DutchTreatApplication
 
 fun main(args: Array<String>) {
